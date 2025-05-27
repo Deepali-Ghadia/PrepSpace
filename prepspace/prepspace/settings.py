ec9949f94311075a112b0f470fcd512b7b0c8bc2
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2025-05-27 23:48:09 +0530

    Set MEDIA_ROOT to syllabus_tracker/media and add MEDIA_URL for serving media files in development

--- prepspace/prepspace/settings.py
+++ prepspace/prepspace/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,7 +87,11 @@ DATABASES = {
     }
 }
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, "syllabus_tracker",'media')
+MEDIA_URL = '/media/'
+print("****************************")
+print(BASE_DIR)
+print("************************")
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
