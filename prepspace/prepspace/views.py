f522a67b350ee98d45195453fe4c9e45d82c84fc
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2025-05-28 17:52:59 +0530

    renders base.html file which is inside theme/templates on prepspace dashboard page

--- prepspace/prepspace/views.py
+++ prepspace/prepspace/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def dashboard(request):
-    return HttpResponse("<H3>Welcome to PrepSpace</h3>")
+    return render(request,'base.html' )
+    # return render(request, 'theme/templates/base.html')
+    # Django will look for base.html inside any templates/ folder of installed apps, like theme/templates/base.html, because of APP_DIRS=True.
